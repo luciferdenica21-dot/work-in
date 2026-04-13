b1be49233f6c93b6165d6ddc852c55bcea3f2c0e
phoenix <luciferdenica21@gmail.com>
2026-04-13 15:48:37 +0400

    fix something

--- server/config/supabase.js
+++ server/config/supabase.js
@@ -1,5 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import process from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
